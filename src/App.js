8a236bacdafdd98dd87ee0b3f6def16f1a4a2fe6
Nabeel Anjum <nabeelanjum888@gmail.com>
2020-11-22 23:47:51 +0500

    Facebook ID added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+} from 'react-native';
+import Login from './screens/Login';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Login />
+    </View>
+  );
+};
+
+export default App;

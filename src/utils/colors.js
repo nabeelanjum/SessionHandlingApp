e51979bcd917793fbd16ba0e923dafb8f6650298
Nabeel Anjum <nabeelanjum888@gmail.com>
2020-11-22 23:49:37 +0500

    Essential constants, colors and functions added

---
+++ src/utils/colors.js
@@ -0,0 +1,9 @@
+export default colors = {
+  TEXT: 'black',
+  WHITE: '#fff',
+  BLACK: '#000',
+  BACKGROUND: '#fff',
+  LIGHT_GREY: 'rgb(240, 240, 240)',
+  LIGHT_YELLOW: 'rgb(245, 245, 230)',
+  FACEBOOK: 'rgb(24, 119, 242)'
+}

fd260594f84ac08e88c2cc4ea4513ef28f169f29
Nabeel Anjum <nabeelanjum888@gmail.com>
2020-11-22 23:50:03 +0500

    Button component added

---
+++ src/components/FacebookLoginButton.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  Pressable,
+  Image,
+  View,
+  StyleSheet,
+  Text,
+} from 'react-native';
+import { normalizeFont, normalizeY } from '../utils/functions';
+import colors from '../utils/colors';
+
+const fbIcon = require('../assets/images/fbLogo.png');
+
+const FacebookLoginButton = ({ onPress }) => (
+  <Pressable style={styles.loginBtn} onPress={onPress}>
+    <Image source={fbIcon} style={styles.fbIcon} />
+    <Text style={styles.loginBtnText}>Login with Facebook</Text>
+  </Pressable>
+)
+
+const styles = StyleSheet.create({
+  loginBtn: {
+    flexDirection: 'row',
+    width: '80%',
+    height: normalizeY(42) > 55 ? 55 : normalizeY(42),
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    borderRadius: 50,
+    shadowOffset: {
+      width: 0,
+      height: 0
+    },
+    shadowOpacity: 0.3,
+    elevation: 5,
+    backgroundColor: colors.FACEBOOK,
+  },
+  loginBtnText: {
+    color: colors.WHITE,
+    fontSize: normalizeFont(12.5)
+  },
+  fbIcon: {
+    width: 40,
+    height: 40,
+    resizeMode: 'contain',
+    // backgroundColor: 'red'
+  }
+})
+
+export default FacebookLoginButton;

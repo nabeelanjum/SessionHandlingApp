e797f0768168440b6188af487107a6983c01c151
Nabeel Anjum <nabeelanjum888@gmail.com>
2020-11-23 04:44:51 +0500

    Thunk added to clean out UI layer

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import AsyncStorage from '@react-native-community/async-storage';
 import { persistStore, persistReducer } from 'redux-persist';
 import appReducer from './appReducer';
+import thunk from 'redux-thunk';
 
 const persistConfig = {
   key: 'root',
@@ -9,11 +10,7 @@ const persistConfig = {
 };
 
 const persistedReducer = persistReducer(persistConfig, appReducer);
-
-const store = createStore(
-  persistedReducer
-);
-
+const store = createStore(persistedReducer, applyMiddleware(thunk));
 const persistor = persistStore(store);
 
 export {

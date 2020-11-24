58b771c2777a540ae1fa4336f30925293f6bb7cf
Nabeel Anjum <nabeelanjum888@gmail.com>
2020-11-24 15:36:48 +0500

    Unused import removed

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,3 @@
-import FacebookLoginButton from './FacebookLoginButton';
-
 export { default as Button } from './Button';
 export { default as Text } from './Text';
 export { default as FBButton } from './FacebookLoginButton';
